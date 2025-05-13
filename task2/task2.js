781f6a19b15157e1a2d5e3b64c08e4ed56e690eb
Maxmillian Muiruri <maxmillianmuiruri@gmail.com>
2025-05-13 16:47:28 +0300

    created a function that checks if a given number is prime using a while loop.

---
+++ task2/task2.js
@@ -0,0 +1,15 @@
+function isPrime(number) {
+  if (number <= 1) {
+    return false;
+  }
+  let i = 2;
+  while (i * i <= number) {
+    if (number % i === 0) {
+      return false;
+    }
+    i++;
+  }
+  return true;
+}
+console.log(isPrime(4));
+console.log(isPrime(3));
